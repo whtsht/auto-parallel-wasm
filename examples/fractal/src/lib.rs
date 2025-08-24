74bf6cee29258d6f29d8cc1c91592500a04db248
whtsht <whiteshirt0079@gmail.com>
2025-08-24 16:03:52 +0900

    add examples

---
+++ examples/fractal/src/lib.rs
@@ -0,0 +1,26 @@
+use num_complex::Complex;
+
+#[no_mangle]
+pub extern "C" fn _start() {
+    let imgx = 800;
+    let imgy = 800;
+
+    let scalex = 3.0 / imgx as f32;
+    let scaley = 3.0 / imgy as f32;
+
+    for x in 0..imgx {
+        for y in 0..imgy {
+            let cx = y as f32 * scalex - 1.5;
+            let cy = x as f32 * scaley - 1.5;
+
+            let c = Complex::new(-0.4, 0.6);
+            let mut z = Complex::new(cx, cy);
+
+            let mut i = 0;
+            while i < 255 && z.norm() <= 2.0 {
+                z = z * z + c;
+                i += 1;
+            }
+        }
+    }
+}
